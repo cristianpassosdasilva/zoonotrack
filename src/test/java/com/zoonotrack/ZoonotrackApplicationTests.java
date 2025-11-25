f394079ec323226614c2b55e5682199da00bfce8
cristianpassosdasilva <hudson.beijo@estudante.iftm.edu.br)>
2025-11-24 21:13:12 -0300

    Criando projeto Crud1

---
+++ src/test/java/com/zoonotrack/ZoonotrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoonotrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoonotrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
